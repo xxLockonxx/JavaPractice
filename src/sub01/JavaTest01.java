6e42572c5454dce73533421d97f538d3840f7d89
USER <USER@121.175.135.224>
2020-05-02 11:03:43 +0900

    자바혼자연습

---
+++ src/sub01/JavaTest01.java
@@ -0,0 +1,13 @@
+package sub01;
+/*
+ * 날짜 : 2020/05/02
+ * 이름 : 이성진 
+ * 제목 : 자바 혼자연습하기
+ */
+
+public class JavaTest01 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+	}
+}
