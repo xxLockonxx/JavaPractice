4e4c4a34795969bfa25ba633e00074a933ef497c
xxLockonxx <ptsgrd@naver.com>
2020-07-03 15:47:11 +0900

    두잇 싱글톤 연습

---
+++ src/doit065Singleton/Company.java
@@ -0,0 +1,12 @@
+package doit065Singleton;
+
+public class Company {
+	private static Company instance = new Company(); // 유일하게 생성한 인스턴스
+	private Company() {}
+	public static Company getInstance() {
+		if(instance == null) {
+			instance = new Company();
+		}
+		return instance;
+	}
+}
