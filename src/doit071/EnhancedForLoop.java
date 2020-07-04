d21c446eea4960a4dc99f7333151f10c7c97cd6b
xxLockonxx <ptsgrd@naver.com>
2020-07-04 14:14:58 +0900

    두잇 배열

---
+++ src/doit071/EnhancedForLoop.java
@@ -0,0 +1,12 @@
+package doit071;
+
+public class EnhancedForLoop {
+	public static void main(String[] args) {
+		String[] strArray = {"Java", "Android", "C", "JavaScript","Python"};
+		
+		for(String lang : strArray) {
+			System.out.println(lang);
+			}
+		}
+		
+}
